5dc0ebe6233101036138dd7b1901459a508020a6
furkan <furkanselvi6@gmail.com>
2016-02-29 03:18:16 +0200

    update

--- dbConnect.php
+++ dbConnect.php
@@ -2,8 +2,8 @@
 session_start();
 ob_start();
 $db_adres='localhost';
-$db_user='root';
-$db_pass='123123124';
+$db_user='user';
+$db_pass='password';
 $db_table='chat';
 try{
     $db = new PDO("mysql:host=$db_adres; dbname=$db_table;charset=utf8","$db_user","$db_pass");
